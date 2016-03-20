700bef47ba3cf0b2bff24f65fe7a565195ca1844
Jim Brower <BulldogLowell@gmail.com>
2016-03-20 13:58:54 -0400

    Update relay-lib.h
    Added various getters/setters same as .cpp mods

--- firmware/relay-lib.h
+++ firmware/relay-lib.h
@@ -1,3 +1,4 @@
+
 #ifndef _RELAY_LIB
 #define _RELAY_LIB
 
@@ -5,37 +6,60 @@
 
 // Based on http://awtfy.com/2011/07/14/a-too-simple-arduino-library-for-handling-the-seeeduino-relay-shield-and-generic-relays/
 
-// Function prototype
+#define MAX_MICRO_DELAY 16383
+
 typedef void (dig_write_func_t)(uint16_t, uint8_t);
 
-// Class Definition.
-class RelayLib
-{
-public:
-  RelayLib() {};  // Empty constructor.  Only for initial setup.
-  RelayLib(int relayPin, int initState=LOW);    // Constructor
-  RelayLib(dig_write_func_t* digitalWrite, int relayPin, int initState=LOW); // Constructor with custom digitalWrite function
+enum RelayAction{
+  ACTIVE_LOW,
+  ACTIVE_HIGH
+};
 
-  // can be used as initialization after empty constructor, or to reset info for existing relay
-  void init(int relayPin, int initState=LOW);
-  void init(dig_write_func_t* digitalWrite, int relayPin, int initState=LOW);
+enum TimeIncrements{
+  USING_MILLIS,
+  USING_MICROS
+};
 
-  void on();    // Turns relay on
-  void off();   // Turns relay off
-  void toggle();  // Toggles state of relay between on and off
-  void pulse(int delayTime=300);  // pulse relay on, then off after delay (in ms).  if allready on, will turn it off after the given delay time
+class RelayLib{
+  public:
+    RelayLib();
+    RelayLib(uint8_t relayPin);
+    void init();
+    void init(const int relayPin);
+    void init(const int relayPin, int initialState, RelayAction action = ACTIVE_HIGH);
+    void init(const int relayPin, int initialState, RelayAction action, uint16_t pulseMillis);
+    void on();
+    void off();
+    void toggle();
+    void pulse();
+    void pulse(unsigned long pulseTime,TimeIncrements timer = USING_MILLIS);
+    void noBlockPulse();
+    void noBlockPulse(unsigned long duration, TimeIncrements timer = USING_MILLIS);
+    void update();
+    void update(unsigned long time);
+    void pulse(int pulseMillis);
+    void setPulse(int pulseTime);
+    bool isRelayOn();
+    bool isRelayOff();
+    bool isOn();
+    bool isOff();
+    //
+    void init(dig_write_func_t* digitalWrite, int relayPin, int _relayState = LOW);
+    RelayLib(dig_write_func_t* digitalWrite, int relayPin, int _relayState = LOW); // Constructor with custom digitalWrite function
 
-  int state();    // returns state of relay (LOW/0/off or HIGH/1/on)
-  bool isRelayOn();   // Returns TRUE if the relay is on , false otherwise
-  bool isRelayOff();  // Returns TRUE if the relay is off, false otherwise
-
-  bool isOn() { return isRelayOn(); }
-  bool isOff() { return isRelayOff(); }
-
-private:
-  int relayState;     // Variables holds on/off state of the relay
-  int relayPin;       // Variable holds which Arduino pin connected to relay.
-  dig_write_func_t* digWrite; // digitalWrite function
+  private:
+    uint8_t _relayPin;
+    bool _relayState;
+    RelayAction _action;
+    uint16_t _pulseTime;
+    unsigned long _lastTime;
+    unsigned long _commandPulseTime;
+    TimeIncrements _timerSelect;
+    void setRelay(int newState);
+    bool getState();
+    bool _activePulse;
+    //
+    dig_write_func_t* _digWrite; // digitalWrite function
 };
 
 #endif
